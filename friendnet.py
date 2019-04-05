8223f5eb9e06a396327a7c31e383d764bcf8a63e
Chris Delaney <cadelaney3@gmail.com>
2019-04-05 13:35:00 -0700

    userExists function added

--- friendnet.py
+++ friendnet.py
@@ -4,7 +4,26 @@ with open("network.json", "r") as read_file:
     network = json.load(read_file)
     network = network['network']
 
-for i in network:
-    print(i['name'])
-    for friend in i['friends']:
-        print(friend['friendName'])
+# for i in network:
+#     print(i['name'])
+#     for friend in i['friends']:
+#         print(friend['friendName'])
+
+class FriendNet:
+    def __init__(self, network):
+        self.network = network['network']
+
+    def userExists(self, username):
+        for i in self.network:
+            if username.lower() in i['name'].lower():
+                return True
+        return False
+
+if __name__ == "__main__":
+
+    with open("network.json", "r") as read_file:
+        network = json.load(read_file)
+        fnet = FriendNet(network)
+    
+    print(fnet.userExists("Molecule man"))
+    
